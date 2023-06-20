616470310576c1406a18937cca01fcd40c75240d
Rayssa Soares <rayssa.soares@tidewise.io>
2023-06-20 15:09:18 -0300

    initial commit by rock-create-orogen

---
+++ water_probe_acquanativa_ap3Types.hpp
@@ -0,0 +1,15 @@
+#ifndef water_probe_acquanativa_ap3_TYPES_HPP
+#define water_probe_acquanativa_ap3_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace water_probe_acquanativa_ap3 {
+}
+
+#endif
+
